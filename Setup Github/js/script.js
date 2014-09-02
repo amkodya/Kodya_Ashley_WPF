353df9660d5364f7a2a36a4a702daa0dc13ce7f9
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-02 17:01:31 -0400

    Prompted user for name
    Prompted user for name

--- Setup Github/js/script.js
+++ Setup Github/js/script.js
@@ -4,4 +4,10 @@ Test JS file
 090214
  */
 
-alert("testing 1...2...3");
+//alert("testing 1...2...3");
+
+
+console.log("Testing the console.log");
+
+var userInput = prompt("Type in your name:");
+console.log(userInput);
