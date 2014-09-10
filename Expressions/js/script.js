3c6631ff7d27f2081c93d3fd23717f2d2049ad5e
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-10 19:52:55 -0400

    pemdas
    pemdas

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,7 +1,15 @@
 /**
  * Created by electrikoala on 9/10/14.
  */
-//expressions - Modulo Operator
+//expressions - PEMDAS
 
-var remainder = 32 % 2;
-console.log(remainder);
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//the average is the sum of the items divided by the number of the items
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+//console.log(average);
+
