21297a1ea43305a56d46ee9239a88736192db2fe
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-10 21:19:50 -0400

    prompt1
    promtpt1

---
+++ Prompts+Alerts/js/script.js
@@ -0,0 +1,8 @@
+//prompt and alert
+
+var width = prompt("we are calculating the area of a rectangle. please enter the width");
+var height = prompt("Please enter the Height");
+
+var area = width * height;
+
+console.log(area); //prints it out to the console
