586b3ba50cbd199c1d3e7079c874054730b4d24a
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-11 23:09:57 -0400

    wacky avergages
    wacky avergaes

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,6 +1,6 @@
 //Expressions_Wacky
 
-// A clown juggling invitational contest is being held at the circus. Clown posse's from all around
+// A clown juggling invitational contest is being held at the state fair. Clown's from surrounding circus'
 // will be coming in to compete. The organizers are trying to order supplies for the contest. To keep
 // everything fair, the organizers are going to give each clown the same clown nose and juggling pins.
 // They need to figure out how many clown noses and juggling pins to purchase to be able to give
@@ -10,15 +10,20 @@
 // how many clowns should be in each clown car.
 
 var clownS = prompt("How many clowns competing?"); // how many clowns?
-var clownPosses = prompt("How many clown posse's?"); //number of clown posse's
-var clownCars = prompt("How many clown cars?"); // number of clown cars
 
 var clownNoses = clownS * 1; //calculating number of clown noses needed to purchase
 var jugglingPins = clownS * 3 ; //calculating number of juggling pins needed
-
 
 console.log(clownNoses); //prints it out to the console
 console.log(jugglingPins); //ptints it out to console
 
 alert(clownNoses + "Clown noses should be purchased"); //result prompt
 alert(jugglingPins + "Juggling Pins should be purchased"); //result prompt
+
+var clownCirc1 = prompt("How many clowns in 1st circus' car?"); //number of clowns in clown circus 1
+var clownCirc2 = prompt("How many clowns in 2nd circus' car?"); //number of clowns in clown circus 2
+var clownCirc3 = prompt("How many clowns in 3rd circus' car?"); //number of clowns in clown circus 3
+var clownCirc4 = prompt("How many clowns in 4th circus' car?");//number of clowns in clown circus 4
+var clownCirc5 = prompt("How many clowns in 5th circus' car?");//number of clowns in clown circus 5
+
+var clownCars = []
