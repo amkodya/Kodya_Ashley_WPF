e5ea94ac4b69e86c7cccfbcceee727e874508bde
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-11 22:42:17 -0400

    industry finalize add modulo
    industry finalize add modulo

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,16 +5,16 @@
 //font faces and images on web pages, make a calculation based on how many font faces
 //and how many images should be used per page.
 
-var webPages = prompt("How many web pages do you have?");
-var clipArt = prompt("How many clip art images do you have?");
-var fontFace = prompt("How many font faces do you have?");
+var webPages = prompt("How many web pages do you have?"); // how many web pages
+var clipArt = prompt("How many clip art images do you have?"); //number of clip art images
+var fontFace = prompt("How many font faces do you have?"); // number of font faces
 
-var clipperPage = clipArt / webPages ;
-var fontperPage = fontFace / webPages ;
+var clipperPage = clipArt % webPages ; //calculating number of clip art images to be used per page modulo
+var fontperPage = fontFace % webPages ; //calculating number of font's per page modulo
 
 console.log(clipperPage); //prints it out to the console
-console.log(fontperPage);
+console.log(fontperPage); //ptints it out to console
 
-alert("You should be using" + clipperPage + "clip art images per web page.");
-alert("You should be using" + fontperPage + "font faces per web page.");
+alert("You should be using" + clipperPage + "clip art images per web page."); //result prompt
+alert("You should be using" + fontperPage + "font faces per web page."); //result promopt
 
--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,1 +1,4 @@
 //Expressions_Wacky
+
+// A clown juggling contest is being held at the circus. The organizers are trying to order supplies
+// for the
