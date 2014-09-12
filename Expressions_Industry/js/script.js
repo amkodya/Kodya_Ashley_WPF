066524451e43aad2e0687a32f81c4ab1aa29ac5d
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-11 22:20:07 -0400

    industry prompts
    industry prompts

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,5 +5,8 @@
 //font faces and images on web pages, make a calculation based on how many font faces
 //and how many images should be used per page
 
+var clipArt = prompt("How many clip art images do you have?");
+var webPages = prompt("How many web pages do you have?");
+var fontFace - prompt("How many font faces do you have?");
 
 
