d7018708d272746879493c2cdfb5b515663a1048
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-11 22:24:52 -0400

    variables industry
    variables industry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,10 +3,13 @@
 // Student is building a website. She needs to have gather images and font faces for
 // all of the web pages she has made for her website. In order to utilize all
 //font faces and images on web pages, make a calculation based on how many font faces
-//and how many images should be used per page
+//and how many images should be used per page.
 
 var clipArt = prompt("How many clip art images do you have?");
 var webPages = prompt("How many web pages do you have?");
-var fontFace - prompt("How many font faces do you have?");
+var fontFace = prompt("How many font faces do you have?");
+
+var clipperPage = webPages / clipArt;
+var fontperPage = webPages / fontFace;
 
 
