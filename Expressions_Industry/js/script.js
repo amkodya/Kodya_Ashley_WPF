6fcbc7e7a6a3f4001893420f9c7f20571ac9de30
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-11 22:19:38 -0400

    industry start
    industry start

---
+++ Expressions_Industry/js/script.js
@@ -0,0 +1,9 @@
+//Expressions_Industry
+
+// Student is building a website. She needs to have gather images and font faces for
+// all of the web pages she has made for her website. In order to utilize all
+//font faces and images on web pages, make a calculation based on how many font faces
+//and how many images should be used per page
+
+
+
