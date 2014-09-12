ad3699740833459c4e83f0291cea737cd4cd6c14
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-11 22:52:05 -0400

    wacky problem
    wacky problem

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,4 +1,12 @@
 //Expressions_Wacky
 
-// A clown juggling contest is being held at the circus. The organizers are trying to order supplies
-// for the
+// A clown juggling invitational contest is being held at the circus. Clown posse's from all around
+// will be coming in to compete. The organizers are trying to order supplies for the contest. To keep
+// everything fair, the organizers are going to give each clown the same clown nose and juggling pins.
+// They need to figure out how many clown noses and juggling pins to purchase to be able to give
+// each clown the same amount. In addition, the clowns will be transported into the competition
+// into clown cars that each posse of clowns came in. The organizers want to put an even amount
+// of clowns into each clown car for show. The average needs to be found out
+// how many clowns should be in each clown car.
+
+
