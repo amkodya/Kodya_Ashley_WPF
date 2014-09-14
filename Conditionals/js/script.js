f1c9e3d336039c8d10ac82155a6aa87a24af30f9
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-14 16:27:12 -0400

    tern 1
    tern1

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,15 +1,16 @@
 /**
  * Created by electrikoala on 9/14/14.
  */
-//CONDITIONAL LOGIC - Logical Operators
+//CONDITIONAL LOGIC - Ternaries
 
-//not operator
+var gpa = 48;
 
-// !
+//if the GPA is over the min 2.0 score, the student can graduate
 
-//true becomes false
+/*if(gpa > 2.0){
+   console.log("you can graduate!");
+}else{
+    console.log("GPA is too low!");
+}*/
 
-var a = 3;
-var b = 5;
-
-a!= b !(a===b);
+(gpa > 2.0) ? console.log("you can graduate!") : console.log("GPA is too low!");
