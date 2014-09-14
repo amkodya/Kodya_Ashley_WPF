25e80cc3871793fb30b26fef933475f156ec9c66
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-14 15:05:57 -0400

    relational with expression

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,15 +1,16 @@
 /**
  * Created by electrikoala on 9/14/14.
  */
-//CONDITIONAL LOGIC - relational expressions
+//CONDITIONAL LOGIC - with an Expression
 
-var kidHeight = 50;
+var kidHeight = 47;
 var minHeight = 48;
+var sneakerLift = 2;
 
 //if the child is old enough, print to the console "you can ride"
-//if the kid is over 48 inches in hight
+//if the kid is over 48 inches in height
 
-if(kidHeight > minHeight){
+if(kidHeight + sneakerLift > minHeight){
     //code performed if condition is true
     console.log("You Can Ride the Coaster!")
 }
