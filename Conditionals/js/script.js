3735f5868623d65d77b0944b9a0fc2077399668f
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-14 16:20:02 -0400

    !=
    !=

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -3,14 +3,13 @@
  */
 //CONDITIONAL LOGIC - Logical Operators
 
-var budget = 300;
-var iPhonePrice = 199.99;
-var wonLottery = true;
+//not operator
 
-//if the price of the phone is less than our budget AND if our paycheck is over 300
-if(iPhonePrice < budget || wonLottery === true){
-    console.log("we Can Buy the Phone!") ;
+// !
 
-}else{
-    console.log("No Phone for you!");
-}
+//true becomes false
+
+var a = 3;
+var b = 5;
+
+a!= b !(a===b);
