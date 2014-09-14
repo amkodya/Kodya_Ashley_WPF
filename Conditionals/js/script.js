b66351870fd978cd290ed19e34d615eafe0ad0f4
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-14 16:10:41 -0400

    ||
    ||

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -5,10 +5,10 @@
 
 var budget = 300;
 var iPhonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
 //if the price of the phone is less than our budget AND if our paycheck is over 300
-if(iPhonePrice < budget && paycheck < 300){
+if(iPhonePrice < budget || wonLottery === true){
     console.log("we Can Buy the Phone!") ;
 
 }else{
