b2191cd5147d34f1e1c4f8fbe96f740fd6e9cb81
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-14 15:15:11 -0400

    else if
    else if

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,10 +1,11 @@
 /**
  * Created by electrikoala on 9/14/14.
  */
-//CONDITIONAL LOGIC - if and else
+//CONDITIONAL LOGIC - else if
 
-var kidHeight = 48;
+var kidHeight = 47;
 var minHeight = 48;
+var wParentHeight = 45; //the height of the kid with a parent
 
 //if the child is old enough, print to the console "you can ride"
 //if the kid is over 48 inches in height
@@ -12,8 +13,10 @@ var minHeight = 48;
 if(kidHeight >= minHeight){
     //code performed if condition is true
     console.log("You Can Ride the Coaster!")
+}else if(kidHeight > wParentHeight){
+     //you can ride with a parent present
+    console.log("You can ride but only with a parent")
 }else{
-     //code performed if condition if false
+    //sorry you have growing to do
     console.log("Sorry kid you've got some growing to do!")
-
-};
+}
