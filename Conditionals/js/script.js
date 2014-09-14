04390804e23c2a9e772aac9c94951655f2369100
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-14 15:08:48 -0400

    if
    if

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,16 +1,19 @@
 /**
  * Created by electrikoala on 9/14/14.
  */
-//CONDITIONAL LOGIC - with an Expression
+//CONDITIONAL LOGIC - if and else
 
-var kidHeight = 47;
+var kidHeight = 48;
 var minHeight = 48;
-var sneakerLift = 2;
 
 //if the child is old enough, print to the console "you can ride"
 //if the kid is over 48 inches in height
 
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight >= minHeight){
     //code performed if condition is true
     console.log("You Can Ride the Coaster!")
 }
+if(kidHeight <= minHeight){
+    console.log("Sorry kid you've got some growing to do!")
+
+};
