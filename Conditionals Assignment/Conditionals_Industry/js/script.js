ab782d28143a2bb7f845efb92e04c2025a0dc17b
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-17 21:17:18 -0400

    console fix
    console fix

--- Conditionals Assignment/Conditionals_Industry/js/script.js
+++ Conditionals Assignment/Conditionals_Industry/js/script.js
@@ -1,10 +1,8 @@
 //Conditionals Industry
 
+/*
 
-
-/*Industry
-
-You am a web designer for a small company. You have a per website budget for stock photos. For the
+You are a web designer for a small company. You have a per-website budget for stock photos. For the
 
 website you're designing, you have selected a certain number of stock photos. Decide whether you can
 
@@ -16,8 +14,7 @@ var budget:100 ;
 var pricePhoto:2.54 ;
 
 if( budget >= (photosSelected * pricePhoto) ){
-
-    console.log(“You can use all the photo's you've collected for this project”);
+    console.log("You can use all of the stock photos")
 
 }else{
 
