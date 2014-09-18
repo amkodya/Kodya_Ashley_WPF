0fe057711c1847cd1a968d9208df65217cc43072
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-17 21:17:47 -0400

    consolefix 2
    console fix 2

--- Conditionals Assignment/Conditionals_Industry/js/script.js
+++ Conditionals Assignment/Conditionals_Industry/js/script.js
@@ -14,12 +14,9 @@ var budget:100 ;
 var pricePhoto:2.54 ;
 
 if( budget >= (photosSelected * pricePhoto) ){
-    console.log("You can use all of the stock photos")
+    console.log("You can use all of the stock photo's you've collected for your website!");
 
 }else{
-
-    console.log(“You need to cut out ”+ ((photosSelected * pricePhoto) – budget) / pricePhoto + “
-
-photos to stay within your budget”);
+    console.log("You need to cut out " + ((photosSelected * pricePhoto) – budget) / pricePhoto + " photo's to stay within your budget");
 
 }
