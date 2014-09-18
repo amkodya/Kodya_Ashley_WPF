71aa6173c5b0be6e64ce33d2250c41f42b64bd0e
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-17 21:30:31 -0400

    personal variable+problem creation
    personal variable+ problem setup

--- Conditionals Assignment/Conditionals_Personal/js/script.js
+++ Conditionals Assignment/Conditionals_Personal/js/script.js
@@ -1,37 +1,15 @@
 //Conditionals Personal
 
-/* Movie Ticket Price
- The local movie theater in town has a ticket price of $12.00
- but if you are a senior (55 and older), under 10 you get the discounted price of $7.00.
- In addition, if a customer is seeing a movie between 3pm and 5pm they can also get the discounted price.
- Determine which of the two prices the customer is eligible for.
+/*I want to make a layered cake for my parents wedding anniversary. All layers will be the same size and will cost the same.
+After purchasing their gift, I only have a certain amount of money to spend on ingredients.
+I want to figure out how many layers I can make based on how much the ingredients are per layer.*/
 
- Given:
- Time of Movie (Assume whole numbers here)
- Age of the customer
- Result To Print Out:
- “The ticket price is X”
- Deliverables
+var cakeMixPrice=5;
+var eggs=5;
+var oil= 3;
+var frosting= 8;
 
- Commit your work to your GitHub.com repository. */
+var money=75;
 
-var grade = 84;
-
-if(grade >= 90){
-    console.log("You have a "+ grade +"%, which means you have earned an A in the class");
-}else if(grade >= 80){
-
-    console.log("You have a "+ grade +"%, which means you have earned a B in the class");
-
-}else if(grade >= 70){
-    console.log("You have a "+ grade +"%, which means you have earned a C in the class");
-
-}else if(grade >= 65){
-    console.log("You have a "+ grade +"%, which means you have earned a D in the class");
-
-}else{
-    console.log("You have a " + grade + "%, which means you have earned an F in the class");
-
-}
 
 
