45b76edd650f4c0e5642483acc2793c6f8e9a40d
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-17 21:31:37 -0400

    elseif+console
    elseif+console

--- Conditionals Assignment/Conditionals_Personal/js/script.js
+++ Conditionals Assignment/Conditionals_Personal/js/script.js
@@ -1,9 +1,10 @@
 //Conditionals Personal
 
-/*I want to make a 3-layered cake for my parents wedding anniversary. All layers will be the same size and will cost the same.
+/*I want to make a 3-layered cake for my parents wedding anniversary.
+All layers will be the same size and will cost the same.
 After purchasing their gift, I only have a certain amount of money to spend on ingredients.
 I want to figure out if I can make a 3 layer cake or if not, how many layers of cake I can make
-based on how much the ingredients are per layer.*/
+based on the cost per layer.*/
 
 var cakeMixPrice=5;
 var eggs=5;
@@ -12,6 +13,9 @@ var frosting= 8;
 
 var money=75;
 
-if(money >= (cakeMixPrice+eggs+oil+frosting)*3;
-    console.log("You can make the 3-Layered cake!")
+if(money >= (cakeMixPrice+eggs+oil+frosting)*3){
+    console.log("You can make the 3-layered cake!");
+}else if(money >= (cakeMixPrice+eggs+oil+frosting)*2){
+    console.log("You can only make a double-layered cake!");
+}
 
