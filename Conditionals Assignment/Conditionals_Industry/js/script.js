4157152afee75be83f51239dedb32d5da89279bd
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-17 21:18:44 -0400

    var correction
    var correction

--- Conditionals Assignment/Conditionals_Industry/js/script.js
+++ Conditionals Assignment/Conditionals_Industry/js/script.js
@@ -9,14 +9,14 @@ website you're designing, you have selected a certain number of stock photos. De
 use the amount of photos you have selected or if you will need to cut some photos out.*/
 
 
-var photosSelected:34 ;
-var budget:100 ;
-var pricePhoto:2.54 ;
+var photosSelected = 240 ;
+var budget = 100 ;
+var pricePhoto = 2.54 ;
 
 if (budget >= photosSelected*pricePhoto){
-    console.log("You can use all of the stock photo's you've collected for your website!");
+    console.log("You can use all of the stock photos you've collected for your website!");
 
 }else{
-    console.log("You need to cut out " + (photosSelected * pricePhoto - budget)/pricePhoto + " photo's to stay within your budget");
+    console.log("You need to cut out " + (photosSelected * pricePhoto - budget)/pricePhoto + " photos to stay within your budget");
 
 }
