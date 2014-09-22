2eabd7ea248015d3515d2cce86f5a8eccaf44b92
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-22 17:03:20 -0400

    function vs procedure
    function vs procedure

--- Screencasts/Functions_Screencasts/js/script.js
+++ Screencasts/Functions_Screencasts/js/script.js
@@ -1,14 +1,17 @@
-//Functions Screencast - Returning Values
+//Functions Screencast - Procedures
 
 /* function functionName( ){
     //code the function runs
 } */
 
-var total = calcArea(30, 20);
-
-function calcArea(w, h){
-   var area = w * h;
-   return area; //function spitting the info out
+//this is a function
+function calcAreaF(width, height){
+    var area = width * height;
+    return area;
 }
-console.log(total);
 
+//this is a procedure
+function calcAreaP(width, height){
+    var area = width * height;
+    console.log(area); //no return
+}
