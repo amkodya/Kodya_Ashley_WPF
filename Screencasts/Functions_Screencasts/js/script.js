961a1b97aeb7f7e2179c46598b8832f04b590367
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-22 16:15:28 -0400

    functions basic structure
    functions basic structure

---
+++ Screencasts/Functions_Screencasts/js/script.js
@@ -0,0 +1,16 @@
+//Functions Screencasts
+
+/* function functionName( ){
+    //code the function runs
+} */
+
+function outptMsg(){
+    console.log("hello World")
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
