82f3bb9c26bb2b955955430e3092e1043e6f13fe
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-25 20:17:10 -0400

    industry problem setup
    industry problem setup

--- Functions Assignment/Functions_Industry/js/script.js
+++ Functions Assignment/Functions_Industry/js/script.js
@@ -1,3 +1,18 @@
-/**
- * Created by electrikoala on 9/25/14.
+//Functions_Industry
+
+/*
+
+ I am being hired to take on an additional design campaign work which will take on extra hours at my job.
+ With the amount of hours I already work, I need to figure out if I can handle the extra workload and
+ still get my current job done and have time for myself. Calculate if I can take on this new position.
+
+ The givens are the
+ How many extra hours will campaign be per day?
+ How many hours a day at current position?
+ Sleep time needed
+ Personal Time needed
+
  */
+
+
+
