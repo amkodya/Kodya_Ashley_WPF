7c64c9d81068f30ea8f60b79062b0bd76d37bdb1
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-09-25 20:35:14 -0400

    wacky problem setup
    wacky problem setup

--- Functions Assignment/Functions_Wacky/js/script.js
+++ Functions Assignment/Functions_Wacky/js/script.js
@@ -1,3 +1,17 @@
-/**
- * Created by electrikoala on 9/25/14.
+//Functions_Wacky
+/*
+
+A space ship full of aliens is descending to the Earth from outer space. They need to figure out
+where they are going to land. They need to land on dry land not water that is larger than their spaceship.
+Also the aliens have heat sensor technology on their ship to tell if humans are nearby
+The aliens need to make sure there are no humans where they are landing that will approach them in fear
+as they are coming to Earth in peace and research.
+
+ The givens are the
+ How long is spaceship
+ Are there are humans around?
+
  */
+
+
+
